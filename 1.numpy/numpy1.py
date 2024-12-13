5a9e4a512e8a4df5be72fcac36455a259e4d3137
gwang1234 <qoi11@naver.com>
2024-12-13 11:27:47 +0900

    넘파이

---
+++ 1.numpy/numpy1.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+# a=np.arange(11) # 0~10까지의 정수로 구성된 1차원 배열 생성 
+# print(a[a<5]) # 5보다 작은 요소만 출력
+# five_up=(a>=5) # 5이상인 요소만 참인 배열 생성
+# print(five_up)
+# print(a[five_up])
+
+# a = np.arange(1,10).reshape(3,3)
+# print(a)
+# print(a.sum())
+# print(np.sum(a))
+# b=a.sum(axis=0) # 열별 합계
+# print(b)
+# c=a.sum(axis=1) # 행별 합계
+# print(c)
+
+# x = np.array([1,2,3,4,5])
+# # y = np.array([1,1,1,1,1])
+# y = 1
+# print(x + y)
+
+# a = np.arange(1,25).reshape(4,6)
+# print(a+100)
+# new_arr = np.full_like(a,100)
+# print(new_arr)
+# print(a+new_arr)
+
+# a = np.arange(5).reshape((1,5))
+# b = np.arange(5).reshape((5,1))
+# print(a)
+# print(b)
+# print(a+b)
+
